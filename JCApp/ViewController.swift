fb0d3f8b2bd97205774c34baf83909faca99a30f
冀明达 <apple@jimingdadeMacBook-Air.local>
2017-06-23 10:08:06 +0800

    Initial Commit

---
+++ JCApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JCApp
+//
+//  Created by 冀明达 on 2017/6/23.
+//  Copyright © 2017年 wxl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
